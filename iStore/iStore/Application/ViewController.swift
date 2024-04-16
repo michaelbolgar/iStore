631965ab5c7444be3668d48446f032f343f92e64
ShirobokovNikita <shirobokov97@yandex.ru>
2024-04-17 00:38:44 +0400

    cocoaInstall

--- iStore/iStore/Application/ViewController.swift
+++ iStore/iStore/Application/ViewController.swift
@@ -1,11 +1,24 @@
 // экран для быстрых тестов разных фич, как например запросы по апи. Можно оставлять тут примеры для других участников команды
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .customRed
+        
+        let testView = UIView()
+        testView.backgroundColor = .black
+        
+        view.addSubview(testView)
+        
+        testView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.width.height.equalTo(100)
+        }
     }
+    
+    
 }
