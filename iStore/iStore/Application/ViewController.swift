0519dc7148dd6e37718228d1f407fa72db136ac2
michaelbolgar <mr.mbolgar@inbox.ru>
2024-04-16 21:37:39 +0200

    deleted storyboard, added UIColor Extension

---
+++ iStore/iStore/Application/ViewController.swift
@@ -0,0 +1,11 @@
+// экран для быстрых тестов разных фич, как например запросы по апи. Можно оставлять тут примеры для других участников команды
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .customRed
+    }
+}
---
+++ iStore/iStore/Extensions/UIColor Extension.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+extension UIColor {
+
+    ///buttons, fill icons, link button
+    static var lightGreen: UIColor = {
+        return UIColor (red: 103/255, green: 196/255, blue: 167/255, alpha: 1.0)
+    }()
+
+    ///success alert or notification
+    static var deepGreen: UIColor = {
+        return UIColor (red: 18/255, green: 183/255, blue: 106/255, alpha: 1.0)
+    }()
+
+    ///paragraph, disable menu, inactive menu
+    static var lightGray: UIColor = {
+        return UIColor (red: 147/255, green: 147/255, blue: 147/255, alpha: 1.0)
+    }()
+
+    ///title, text button, text field label
+    static var darkGray: UIColor = {
+        return UIColor (red: 57/255, green: 63/255, blue: 66/255, alpha: 1.0)
+    }()
+
+    ///error text or message
+    static var customRed: UIColor = {
+        return UIColor (red: 214/255, green: 91/255, blue: 91/255, alpha: 1.0)
+    }()
+
+    ///buttons, fill icons, link button
+    static var customYellow: UIColor = {
+        return UIColor (red: 247/255, green: 144/255, blue: 8/255, alpha: 1.0)
+    }()
+}
