6a31d2437a7caabf8646512cd762246e1b33a44c
SemenShevchik <145438049+SemenShevchik@users.noreply.github.com>
2024-04-22 00:58:50 +0300

    Add profile VC (#21)
    * Add profile VC
    
    * add changeProfileVC
    
    ---------
    
    Co-authored-by: Michael Bolgar <119865051+michaelbolgar@users.noreply.github.com>

--- iStore/iStore/Extensions/UIView Extension.swift
+++ iStore/iStore/Extensions/UIView Extension.swift
@@ -1,11 +1,50 @@
 import UIKit
 
 extension UIView {
-
+    
     /// needs to be added to every screen contains a text field
     func hideKeyboard() {
         let tap: UIGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIView.endEditing))
         self.addGestureRecognizer(tap)
         tap.cancelsTouchesInView = false //иначе при тапе на ячейку задержка в несколько секунд
+    }
+    
+    static func makeView(textLabel: String, textColor: UIColor, nameMarker: String, colorMarker: UIColor) -> UIView {
+        
+        let view = UIView()
+        
+        view.layer.cornerRadius = 12
+        view.backgroundColor = UIColor.lightViolet
+        view.tintColor = UIColor.darkGray
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Создание и настройка надписи
+        let label = UILabel.makeLabel(text: textLabel,
+                                      font: UIFont.systemFont(ofSize: 16, weight: .semibold),
+                                      textColor: textColor,
+                                      numberOfLines: 1,
+                                      alignment: .left)
+        
+        // Создание и настройка метки
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: nameMarker)
+        imageView.contentMode = .scaleAspectFit
+        imageView.tintColor = colorMarker
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(label)
+        view.addSubview(imageView)
+        
+        NSLayoutConstraint.activate([
+            
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            
+            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            imageView.centerXAnchor.constraint(equalTo: view.trailingAnchor, constant: -25),
+            
+            view.heightAnchor.constraint(equalToConstant: 56)
+        ])
+        return view
     }
 }
---
+++ iStore/iStore/Moduls/Tabbar Moduls/Profile/View/ProfileVС.swift
@@ -0,0 +1,181 @@
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    // MARK: - UI Elements
+    private let profileTitle = UILabel.makeLabel(text: "Profile",
+                                                 font: .InterBold(ofSize: 24),
+                                                 textColor: .customDarkGray,
+                                                 numberOfLines: nil,
+                                                 alignment: .center)
+    
+    private lazy var profileImage: UIImageView = {
+        let element = UIImageView()
+        element.image = UIImage(named: "profilePhoto")
+        element.layer.cornerRadius = 65
+        element.clipsToBounds = true
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private let profileName = UILabel.makeLabel(text: "Mstr. Anderson",
+                                                font: .InterSemiBold(ofSize: 20),
+                                                textColor: .customDarkGray,
+                                                numberOfLines: 1,
+                                                alignment: .left)
+    
+    private let profileEmail = UILabel.makeLabel(text: "dev@gmail.com",
+                                                 font: .InterSemiBold(ofSize: 14),
+                                                 textColor: .customLightGray,
+                                                 numberOfLines: 1,
+                                                 alignment: .left)
+    
+    private lazy var settingsProfileButton: UIButton = {
+        let element = UIButton(type: .system)
+        element.setBackgroundImage(UIImage(systemName: "gearshape.2"), for: .normal)
+        element.backgroundColor = .white
+        element.tintColor = .customDarkGray
+        element.addTarget(self, action: #selector(settingsProfileButtonTapped), for: .touchUpInside)
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    
+    private lazy var changePhotoProfileButton: UIButton = {
+        let element = UIButton(type: .system)
+        element.setBackgroundImage(UIImage(systemName: "square.and.pencil.circle.fill"), for: .normal)
+        element.layer.borderColor = UIColor.white.cgColor
+        element.layer.borderWidth = 3
+        element.layer.cornerRadius = 17
+        element.backgroundColor = .white
+        element.addTarget(self, action: #selector(changePhotoProfileButtonTapped), for: .touchUpInside)
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private let typeAccountView = UIView.makeView(textLabel: "Type of account", 
+                                                  textColor: .customDarkGray,
+                                                  nameMarker: "chevron.forward", 
+                                                  colorMarker: .customDarkGray)
+    
+    private let termsView = UIView.makeView(textLabel: "Terms & Conditions", 
+                                            textColor: .customDarkGray,
+                                            nameMarker: "chevron.forward",
+                                            colorMarker: .customDarkGray)
+    
+    private let signoutView = UIView.makeView(textLabel: "Sign Out",
+                                              textColor: .customDarkGray,
+                                              nameMarker: "arrow.forward.to.line.square", 
+                                              colorMarker: .customDarkGray)
+    
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+        setupConstraints()
+    }
+    
+    //MARK: Private Methods
+    private func setupViews() {
+        
+        view.backgroundColor = .white
+        
+        view.hideKeyboard() // это нужно для реализации функции по изменению логина и почты
+     
+        [profileTitle, settingsProfileButton, profileImage, profileName, profileEmail, changePhotoProfileButton, typeAccountView, termsView, signoutView].forEach { view.addSubview($0) }
+        
+        //добавляем рекогнайзер на кнопки(вью)
+        typeAccountView.isUserInteractionEnabled = true
+        termsView.isUserInteractionEnabled = true
+        signoutView.isUserInteractionEnabled = true
+        
+        let typeAccountTapGesture = UITapGestureRecognizer(target: self, action: #selector(typeAccountViewTapped))
+        typeAccountView.addGestureRecognizer(typeAccountTapGesture)
+        
+        let termsTapGesture = UITapGestureRecognizer(target: self, action: #selector(termsViewTapped))
+        termsView.addGestureRecognizer(termsTapGesture)
+        
+        let signoutTapGesture = UITapGestureRecognizer(target: self, action: #selector(signoutViewTapped))
+        signoutView.addGestureRecognizer(signoutTapGesture)
+        
+    }
+    
+    // MARK: Selector Methods
+    @objc private func settingsProfileButtonTapped() {
+        print("settingsProfile button tapped")
+    }
+    
+    @objc private func changePhotoProfileButtonTapped() {
+        let changePhotoVC = ChangePhotoViewController()
+        changePhotoVC.modalPresentationStyle = .automatic
+        present(changePhotoVC, animated: true, completion: nil)
+    }
+    
+    @objc private func typeAccountViewTapped() {
+        let changePhotoVC = ChangeProfileViewController()
+        changePhotoVC.modalPresentationStyle = .automatic
+        present(changePhotoVC, animated: true, completion: nil)
+    }
+    
+    @objc private func termsViewTapped() {
+        print("terms button tapped")
+        let termsVC = TermsViewController()
+        termsVC.modalTransitionStyle = .coverVertical
+        present(termsVC, animated: true, completion: nil)
+    }
+    
+    @objc private func signoutViewTapped() {
+        print("signout button tapped")
+    }
+}
+
+// MARK: - Setup Constraints
+private extension ProfileViewController {
+    
+    func setupConstraints() {
+        // Определение константы для удобства
+        let inset: CGFloat = 20
+        
+        NSLayoutConstraint.activate([
+            
+            profileTitle.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            profileTitle.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            settingsProfileButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            settingsProfileButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -inset),
+            
+            profileImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 92),
+            profileImage.leftAnchor.constraint(equalTo: view.leftAnchor, constant: inset),
+            profileImage.widthAnchor.constraint(equalToConstant: 130),
+            profileImage.heightAnchor.constraint(equalToConstant: 130),
+            
+            changePhotoProfileButton.widthAnchor.constraint(equalToConstant: 34),
+            changePhotoProfileButton.heightAnchor.constraint(equalToConstant: 34),
+            changePhotoProfileButton.trailingAnchor.constraint(equalTo: profileImage.trailingAnchor),
+            changePhotoProfileButton.bottomAnchor.constraint(equalTo: profileImage.bottomAnchor),
+            
+            profileName.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 104),
+            profileName.leadingAnchor.constraint(equalTo: profileImage.leadingAnchor, constant: 150),
+            profileName.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -inset),
+            profileName.heightAnchor.constraint(equalToConstant: 24),
+            
+            profileEmail.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 128),
+            profileEmail.leadingAnchor.constraint(equalTo: profileImage.leadingAnchor, constant: 150),
+            profileEmail.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -inset),
+            profileEmail.heightAnchor.constraint(equalToConstant: 24),
+            
+            typeAccountView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: inset),
+            typeAccountView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -inset),
+            typeAccountView.bottomAnchor.constraint(equalTo: termsView.bottomAnchor, constant: -84),
+            
+            termsView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: inset),
+            termsView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -inset),
+            termsView.bottomAnchor.constraint(equalTo: signoutView.bottomAnchor, constant: -84),
+            
+            signoutView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: inset),
+            signoutView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -inset),
+            signoutView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -124)
+        ])
+    }
+}
+
