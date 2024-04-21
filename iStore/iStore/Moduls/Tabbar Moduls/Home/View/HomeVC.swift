17efd75e9a725d31536deab6e92e1f863cfcddc4
Michael Bolgar <119865051+michaelbolgar@users.noreply.github.com>
2024-04-21 23:55:48 +0200

    Michaelbolgar builders (#29)
    * started work with builders
    
    * created tabbar & tabbar builders
    
    * fixed tabbar icons
    
    * renamed (main) builder into factory

--- iStore/iStore/Moduls/Tabbar Moduls/Home/View/HomeVC.swift
+++ iStore/iStore/Moduls/Tabbar Moduls/Home/View/HomeVC.swift
@@ -1,6 +1,12 @@
 import UIKit
 
+protocol HomeVCProtocol: AnyObject {
+//    func reloadCollectionView(section: Int)
+}
+
 final class HomeVC: UIViewController {
+
+    var presenter: HomePresenterProtocol!
 
     override func viewDidLoad() {
         super.viewDidLoad()
