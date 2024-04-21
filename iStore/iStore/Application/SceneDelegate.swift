1e9925e8a2e3debfd96fda33c129b869ce0658fd
Maryna Bolotska <m.bolotska@icloud.com>
2024-04-21 21:58:23 +0200

    added DetailsVC

--- iStore/iStore/Application/SceneDelegate.swift
+++ iStore/iStore/Application/SceneDelegate.swift
@@ -4,12 +4,24 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
+//    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+//        guard let windowScene = (scene as? UIWindowScene) else { return }
+//
+//        let window = UIWindow(windowScene: windowScene)
+//        window.rootViewController = ViewController()
+//        window.makeKeyAndVisible()
+//        self.window = window
+//    }
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }
+        guard let _ = (scene as? UIWindowScene) else { return }
 
+        guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = ViewController()
+        let startVC = DetailsVC()
+        let navigationController = UINavigationController(rootViewController: startVC)
+        window.rootViewController = navigationController
         window.makeKeyAndVisible()
+
         self.window = window
-    }
+        }
 }
