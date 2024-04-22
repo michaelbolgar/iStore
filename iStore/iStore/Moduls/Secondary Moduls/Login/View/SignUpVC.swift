06fa6cc2c39d6a62afd83b70dde40b46d8547763
michaelbolgar <mr.mbolgar@inbox.ru>
2024-04-22 10:59:10 +0200

    cleaned code

--- iStore/iStore/Moduls/Secondary Moduls/Login/View/SignUpVC.swift
+++ iStore/iStore/Moduls/Secondary Moduls/Login/View/SignUpVC.swift
@@ -222,79 +222,6 @@ extension SignUpVC: UITextFieldDelegate {
     }
 }
 
-// Layouts
-extension SignUpVC {
-    private func setupLayout() {
-        let standardSpacing: CGFloat = 20
-        let closeSpacing: CGFloat = 5
-        let buttonHeight: CGFloat = 57
-        let sidePadding: CGFloat = 20
-        
-        NSLayoutConstraint.activate([
-            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-            
-            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
-            contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
-            contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
-            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
-            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
-            
-            completeAccountLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: standardSpacing),
-            completeAccountLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
-            
-            loginLabel.topAnchor.constraint(equalTo: completeAccountLabel.bottomAnchor, constant: standardSpacing),
-            loginLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: sidePadding),
-            loginLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -sidePadding),
-            
-            loginTextField.topAnchor.constraint(equalTo: loginLabel.bottomAnchor, constant: closeSpacing),
-            loginTextField.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: sidePadding),
-            loginTextField.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -sidePadding),
-            
-            emailLabel.topAnchor.constraint(equalTo: loginTextField.bottomAnchor, constant: standardSpacing),
-            emailLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: sidePadding),
-            emailLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -sidePadding),
-            
-            emailTextField.topAnchor.constraint(equalTo: emailLabel.bottomAnchor, constant: closeSpacing),
-            emailTextField.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: sidePadding),
-            emailTextField.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -sidePadding),
-            
-            passwordLabel.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: standardSpacing),
-            passwordLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: sidePadding),
-            passwordLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -sidePadding),
-            
-            passwordTextField.topAnchor.constraint(equalTo: passwordLabel.bottomAnchor, constant: closeSpacing),
-            passwordTextField.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: sidePadding),
-            passwordTextField.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -sidePadding),
-            
-            confirmPasswordLabel.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: standardSpacing),
-            confirmPasswordLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: sidePadding),
-            confirmPasswordLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -sidePadding),
-            
-            confirmPasswordTextField.topAnchor.constraint(equalTo: confirmPasswordLabel.bottomAnchor, constant: closeSpacing),
-            confirmPasswordTextField.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: sidePadding),
-            confirmPasswordTextField.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -sidePadding),
-            
-            accountTypeButton.topAnchor.constraint(equalTo: confirmPasswordTextField.bottomAnchor, constant: standardSpacing),
-            accountTypeButton.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: sidePadding),
-            accountTypeButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -sidePadding),
-            
-            signUpButton.topAnchor.constraint(equalTo: accountTypeButton.bottomAnchor, constant: standardSpacing),
-            signUpButton.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: sidePadding),
-            signUpButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -sidePadding),
-            signUpButton.heightAnchor.constraint(equalToConstant: buttonHeight),
-            
-            accountLoginStack.topAnchor.constraint(equalTo: signUpButton.bottomAnchor, constant: standardSpacing),
-            accountLoginStack.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
-            accountLoginStack.leadingAnchor.constraint(greaterThanOrEqualTo: contentView.leadingAnchor, constant: sidePadding),
-            accountLoginStack.trailingAnchor.constraint(lessThanOrEqualTo: contentView.trailingAnchor, constant: -sidePadding),
-            accountLoginStack.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -standardSpacing)
-        ])
-    }
-}
-
 // End editing abd dismiss keyboard
 extension SignUpVC {
     
@@ -368,5 +295,79 @@ extension UIView {
             view.heightAnchor.constraint(equalToConstant: 56)
         ])
         return view
+    }
+}
+
+// MARK: Layout
+
+extension SignUpVC {
+    private func setupLayout() {
+        let standardSpacing: CGFloat = 20
+        let closeSpacing: CGFloat = 5
+        let buttonHeight: CGFloat = 57
+        let sidePadding: CGFloat = 20
+
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+
+            completeAccountLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: standardSpacing),
+            completeAccountLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+
+            loginLabel.topAnchor.constraint(equalTo: completeAccountLabel.bottomAnchor, constant: standardSpacing),
+            loginLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: sidePadding),
+            loginLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -sidePadding),
+
+            loginTextField.topAnchor.constraint(equalTo: loginLabel.bottomAnchor, constant: closeSpacing),
+            loginTextField.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: sidePadding),
+            loginTextField.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -sidePadding),
+
+            emailLabel.topAnchor.constraint(equalTo: loginTextField.bottomAnchor, constant: standardSpacing),
+            emailLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: sidePadding),
+            emailLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -sidePadding),
+
+            emailTextField.topAnchor.constraint(equalTo: emailLabel.bottomAnchor, constant: closeSpacing),
+            emailTextField.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: sidePadding),
+            emailTextField.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -sidePadding),
+
+            passwordLabel.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: standardSpacing),
+            passwordLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: sidePadding),
+            passwordLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -sidePadding),
+
+            passwordTextField.topAnchor.constraint(equalTo: passwordLabel.bottomAnchor, constant: closeSpacing),
+            passwordTextField.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: sidePadding),
+            passwordTextField.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -sidePadding),
+
+            confirmPasswordLabel.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: standardSpacing),
+            confirmPasswordLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: sidePadding),
+            confirmPasswordLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -sidePadding),
+
+            confirmPasswordTextField.topAnchor.constraint(equalTo: confirmPasswordLabel.bottomAnchor, constant: closeSpacing),
+            confirmPasswordTextField.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: sidePadding),
+            confirmPasswordTextField.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -sidePadding),
+
+            accountTypeButton.topAnchor.constraint(equalTo: confirmPasswordTextField.bottomAnchor, constant: standardSpacing),
+            accountTypeButton.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: sidePadding),
+            accountTypeButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -sidePadding),
+
+            signUpButton.topAnchor.constraint(equalTo: accountTypeButton.bottomAnchor, constant: standardSpacing),
+            signUpButton.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: sidePadding),
+            signUpButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -sidePadding),
+            signUpButton.heightAnchor.constraint(equalToConstant: buttonHeight),
+
+            accountLoginStack.topAnchor.constraint(equalTo: signUpButton.bottomAnchor, constant: standardSpacing),
+            accountLoginStack.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            accountLoginStack.leadingAnchor.constraint(greaterThanOrEqualTo: contentView.leadingAnchor, constant: sidePadding),
+            accountLoginStack.trailingAnchor.constraint(lessThanOrEqualTo: contentView.trailingAnchor, constant: -sidePadding),
+            accountLoginStack.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -standardSpacing)
+        ])
     }
 }
