c27f5e6a4aa95425d12441684bf328fe9ab2bfb0
Alexander Altman <qewhouse@gmail.com>
2024-04-25 02:13:08 +0300

    SearchBar refactoring + Wishlist refactor (#50)
    * SearchBar refactoring + Wishlist refactor
    
    + searchBar init change from Frame-based to AutoLayout
    + add cell shadow to UIView extension
    + add button tap animation to UIButton extension
    
    * Empty collection state

--- iStore/iStore/Extensions/UISearchBar Extension.swift
+++ iStore/iStore/Extensions/UISearchBar Extension.swift
@@ -10,23 +10,23 @@ extension SearchBarView: UISearchBarDelegate {
         toggleSearchButton(with: !searchText.isEmpty)
         delegate?.searchBar(searchBar, textDidChange: searchText)
     }
-
+    
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         dismiss(searchBar)
         delegate?.searchBarSearchButtonClicked(searchBar)
     }
-
+    
     func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
         toggleSearchButton(with: !(searchBar.text?.isEmpty ?? true))
         searchBar.setShowsCancelButton(true, animated: true)
         return true
     }
-
+    
     func searchBarShouldEndEditing(_ searchBar: UISearchBar) -> Bool {
         toggleSearchButton(with: false)
         return true
     }
-
+    
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
         cancelAction(with: searchBar)
     }
@@ -39,17 +39,17 @@ extension SearchBarView {
             animatable()
         }
     }
-
+    
     private func dismiss(_ searchBar: UISearchBar) {
         searchBar.endEditing(true)
     }
-
+    
     private func cancelAction(with searchBar: UISearchBar) {
         withAnimation { [self] in
             searchBar.text = nil
             searchBar.showsCancelButton = false
             dismiss(searchBar)
-
+            
             layoutIfNeeded()
         }
     }
@@ -64,7 +64,7 @@ protocol SearchBarViewDelegate: AnyObject {
 final class SearchBarView: UIView {
     weak var delegate: SearchBarViewDelegate?
     var searchIcon: String = "magnifyingglass"
-
+    
     // MARK: - UI Properties
     private lazy var searchBar: UISearchBar = {
         let search = UISearchBar()
@@ -77,9 +77,17 @@ final class SearchBarView: UIView {
         search.searchTextField.placeholder = "Search"
         search.searchTextField.textColor = .gray
         search.backgroundColor = .clear
+        search.searchTextField.backgroundColor = .white
+        search.searchTextField.layer.borderColor = UIColor.customLightGray.withAlphaComponent(0.5).cgColor
+        search.searchTextField.layer.borderWidth = 1
+        search.searchTextField.layer.cornerRadius = 8
+
+        // Set the background image of the searchBar to a transparent image
+        search.backgroundImage = UIImage()
+        
         return search
     }()
-
+    
     private lazy var searchButton: UIButton = {
         let image = UIImage(systemName: searchIcon)
         let action = UIAction(image: image, handler: searchAction)
@@ -87,46 +95,54 @@ final class SearchBarView: UIView {
         button.isEnabled = false
         return button
     }()
-
+    
     // MARK: - Initializators
-    override init(frame: CGRect) {
+    // Initial code commented by Qew
+    //    override init(frame: CGRect) {
+    //        super.init(frame: frame)
+    //        setView()
+    //        setupConstraints()
+    //    }
+    
+    //Qew refactored code
+    override init(frame: CGRect = .zero) {
         super.init(frame: frame)
         setView()
         setupConstraints()
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     // MARK: - Layout Private Methods
     private func setView() {
         addSubview(searchBar)
         searchBar.addSubview(searchButton)
     }
-
- private func setupConstraints() {
-    searchBar.translatesAutoresizingMaskIntoConstraints = false
-    searchButton.translatesAutoresizingMaskIntoConstraints = false
-
-    NSLayoutConstraint.activate([
-        // Constraints for searchBar to fill its superview
-        searchBar.topAnchor.constraint(equalTo: topAnchor),
-        searchBar.leadingAnchor.constraint(equalTo: leadingAnchor),
-        searchBar.trailingAnchor.constraint(equalTo: trailingAnchor),
-        searchBar.bottomAnchor.constraint(equalTo: bottomAnchor),
-
-        // Constraints for searchButton
-        searchButton.topAnchor.constraint(equalTo: searchBar.searchTextField.topAnchor, constant: 5),
-        searchButton.leadingAnchor.constraint(equalTo: searchBar.searchTextField.leadingAnchor, constant: 5)
-    ])
-}
-
+    
+    private func setupConstraints() {
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        searchButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            // Constraints for searchBar to fill its superview
+            searchBar.topAnchor.constraint(equalTo: topAnchor),
+            searchBar.leadingAnchor.constraint(equalTo: leadingAnchor),
+            searchBar.trailingAnchor.constraint(equalTo: trailingAnchor),
+            searchBar.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+            // Constraints for searchButton
+            searchButton.topAnchor.constraint(equalTo: searchBar.searchTextField.topAnchor, constant: 5),
+            searchButton.leadingAnchor.constraint(equalTo: searchBar.searchTextField.leadingAnchor, constant: 5)
+        ])
+    }
+    
     // MARK: - Private Methods
     private func searchAction(_ action: UIAction) {
         searchBarSearchButtonClicked(searchBar)
     }
-
+    
     // MARK: - Internal Methods
     func toggleSearchButton(with value: Bool) {
         searchButton.isEnabled = value
