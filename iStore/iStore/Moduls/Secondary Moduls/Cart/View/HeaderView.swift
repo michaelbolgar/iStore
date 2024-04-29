7d9217e5ec5dd753aa6c7914d4c0b938f28d6c35
Maryna B <42913418+mabolotska@users.noreply.github.com>
2024-04-29 17:16:33 +0200

    Mabolotska cart (#80)
    * add CartVC
    
    * fixed CartVC
    
    * fix
    
    * add calculation
    
    ---------
    
    Co-authored-by: Michael Bolgar <119865051+michaelbolgar@users.noreply.github.com>

---
+++ iStore/iStore/Moduls/Secondary Moduls/Cart/View/HeaderView.swift
@@ -0,0 +1,58 @@
+//
+//  HeaderView.swift
+//  iStore
+//
+
+import UIKit
+
+class HeaderView: UITableViewHeaderFooterView {
+    private let deliveryLabel = UILabel.makeLabel(text: "Delivery to",
+                                                  font: UIFont.InterMedium(ofSize: 13),
+                                                  textColor: UIColor.customDarkGray,
+                                                  numberOfLines: 1,
+                                                  alignment: .left)
+    
+    private let addressLabel = UILabel.makeLabel(text: "Salatiaga City, Central Java",
+                                                 font: UIFont.InterMedium(ofSize: 13),
+                                                 textColor: UIColor.customDarkGray,
+                                                 numberOfLines: 1,
+                                                 alignment: .left)
+    private let arrowImage: UIImageView = {
+        let image = UIImageView()
+        image.contentMode = .scaleAspectFit
+        image.image = UIImage(named: "ArrowDown")
+        return image
+    }()
+
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        addSubview(deliveryLabel)
+        addSubview(addressLabel)
+        addSubview(arrowImage)
+        arrowImage.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            deliveryLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 23),
+            deliveryLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -15),
+            deliveryLabel.topAnchor.constraint(equalTo: topAnchor, constant: 15),
+
+            addressLabel.trailingAnchor.constraint(equalTo: arrowImage.leadingAnchor, constant: -10),
+            addressLabel.centerYAnchor.constraint(equalTo: deliveryLabel.centerYAnchor),
+
+            arrowImage.centerYAnchor.constraint(equalTo: deliveryLabel.centerYAnchor),
+            arrowImage.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -23),
+            arrowImage.heightAnchor.constraint(equalToConstant: 17),
+        ])
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        addBorder(y: 0)
+        addBorder(y: bounds.height - 1.0)
+    }
+
+}
